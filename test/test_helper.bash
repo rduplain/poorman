c2a372e72fcbc217ec4a7667fde6826ec3c31a2b
Ron DuPlain <ron.duplain@gmail.com>
2015-01-24 15:45:58 -0500

    Add test assertions, prepare for fixtures.
    These come from otherwise rejected commits.

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,3 +1,13 @@
+fixture() {
+    export FIXTURE_ROOT="$BATS_TEST_DIRNAME/fixture/$1"
+}
+
+setup() {
+    if [ -n "$FIXTURE_ROOT" ]; then
+        cd $FIXTURE_ROOT
+    fi
+}
+
 assert_equal() {
     local test=$1
     local expected=$2
@@ -28,4 +38,31 @@ assert_does_not_exist() {
         echo "failure:  $file exists" >&2
         return 1
     fi
+}
+
+assert_not_equal() {
+    assert_equal "number of arguments" "3" "$#"
+    local test=$1
+    local expected=$2
+    local actual=$3
+    if [ "$expected" == "$actual" ]; then
+        echo "test:       $test" >&2
+        echo "unexpected: $actual" >&2
+        return 1
+    fi
+}
+
+assert_is_number() {
+    assert_equal "number of arguments" "2" "$#"
+    local test=$1
+    local value=$2
+    case $value in
+        ''|*[!0-9]*)
+            echo "test:         $test" >&2
+            echo "not a number: $value" >&2
+            return 1
+            ;;
+        *)
+            ;;
+    esac
 }
